0a12abbd1b52142d187cddb9050a0a7a14297b09
tristan <tristan@tristans.cloud>
2024-10-22 23:31:45 +0100

    add example usage

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Cosmic Home-manager Module
 
-This module is a work-in-progress.
-Feel free to use it in your config and provide feedback to
-the nixos cosmic matrix room: [#cosmic:nixos.org](https://matrix.to/#/#cosmic:nixos.org)
-or to me [@tristan:tristans.cloud](matrix://u/tristan:tristans.cloud)
+This module is a work-in-progress. Expect breaking changes.
 
-If you read the flake, you will see that it's just a one liner,
-but might be convenient.
+Having said that, feel free to use it in your config and provide feedback to
+the nixos cosmic matrix room: [#cosmic:nixos.org](https://matrix.to/#/#cosmic:nixos.org)
+or to me [@tristan:tristans.cloud](matrix://u/tristan:tristans.cloud).
 
 ## Usage:
 
@@ -20,27 +18,27 @@ Add this repo to your flake inputs
 }
 ```
 
-and to your home-manager config add the module to your imports
+and to your home-manager config add the module to your modules
 
 ```nix
-imports = [
+modules = [
     inputs.home-manager-cosmic.homeManagerModules.cosmic
 ]
 ```
 
-alternatively, if you are using the home-manager NixOS Module,
+or imports.
+
+Alternatively, if you are using the home-manager NixOS Module,
 you can add this to `home-manager.sharedModules` ([docs](https://nix-community.github.io/home-manager/nixos-options.xhtml#nixos-opt-home-manager.sharedModules)).
 
 To get the updated docs with the cosmic options included:
 
 ```sh
-nix build github:tristanbeedell/home-manager/cosmic#docs-manpages
+nix build github:tristanbeedell/hm-cosmic#docs-manpages
 man result/share/man/man5/home-configuration.nix.5
 ```
 
 The added options are under `programs.cosmic`.
 
-If you want, you could replace home-manager with this flake.
-But that might be rather fickle as you won't get upstream's updates
-so often.
+Also, see [example.nix](./example.nix)
 
--- flake.nix
+++ flake.nix
@@ -1,28 +1,63 @@
 {
   inputs = {
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager-cosmic.url = "github:tristanbeedell/home-manager/cosmic";
   };
 
-  outputs = {home-manager-cosmic, ...}:
-  home-manager-cosmic // {
+  outputs = { home-manager-cosmic, home-manager, self, ... }:
+    home-manager-cosmic // {
+      homeManagerModules.cosmic = { lib, ... }: {
+        imports = [
+          (import "${home-manager-cosmic}/modules/programs/cosmic/.")
+        ]
+        /* there's no use adding this to the actual module,
+           but for all you early birds, I'll try and keep this updated.
+        */
+          ++ (map (name:
+            lib.mkRenamedOptionModule [ "programs" "cosmic" "comp" name ] [
+              "programs"
+              "cosmic"
+              "comp"
+              "settings"
+              name
+            ]) [
+              "active_hint"
+              "autotile"
+              "autotile_behavior"
+              "cursor_follows_focus"
+              "descale_xwayland"
+              "focus_follows_cursor"
+              "focus_follows_cursor_delay"
+              "input_default"
+              "input_devices"
+              "input_touchpad"
+              "workspaces"
+            ]) ++ [
+              (lib.mkRemovedOptionModule [
+                "programs"
+                "cosmic"
+                "comp"
+                "xkb_config"
+              ] ''
+                Cosmic likes overriding this file.
+                It will automatically override it with your system config at runtime.
+              '')
+            ];
+      };
 
-    homeManagerModules.cosmic = {lib, ...}: {
-      imports = [
-        (import "${home-manager-cosmic}/modules/programs/cosmic/.")
-        # (lib.mkRenamedOptionModule ["programs.cosmic"] [])
-      ];
+      packages.x86_64-linux.homeConfigurations.example =
+        let pkgs = home-manager.inputs.nixpkgs.legacyPackages.x86_64-linux;
+        in home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home.stateVersion = "24.05";
+              home.username = "example";
+              home.homeDirectory = "/example";
+            }
+            self.homeManagerModules.cosmic
+            ./example.nix
+          ];
+        };
     };
-
-    packages.x86_64-linux.homeConfigurations.example = let
-      pkgs = home-manager-cosmic.inputs.nixpkgs.legacyPackages.x86_64-linux;
-    in home-manager-cosmic.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [{
-        home.stateVersion = "24.05";
-        home.username = "example";
-        home.homeDirectory = "/example";
-      }
-      ./example.nix];
-    };
-  };
 }
